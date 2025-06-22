a3fd91a8c21075cba1b93075ab1a085fa7bd976f
ensariskin <ensariskin@gmail.com>
2025-06-22 14:22:34 +0300

    ...

--- digital/modules/common/src/D_FF_block.sv
+++ digital/modules/common/src/D_FF_block.sv
@@ -1,18 +1,17 @@
 `timescale 1ns/1ns
 
-module D_FF_block#(parameter mem_width = 16, parameter mem_depth = 16)(
+module D_FF_block #(parameter mem_width = 16, parameter mem_depth = 16)(
     input clk,
     input reset,
     input [mem_width-1 : 0] Rin,
     input we,
     input [mem_depth-1:0] S,
     output [mem_width*mem_depth-1:0] Rout);
-    
+
     genvar i;
-    generate 
+    generate
     for(i = 0; i < mem_depth; i = i+1) begin
-        defparam DFF.mem_width = mem_width;
-        D_FF_async_rst DFF(
+        D_FF_async_rst #(.mem_width(mem_width)) DFF(
         .clk(clk),
         .reset(reset),
         .Rin(Rin),
@@ -21,5 +20,5 @@ module D_FF_block#(parameter mem_width = 16, parameter mem_depth = 16)(
         );
     end
     endgenerate
-    
+
 endmodule
