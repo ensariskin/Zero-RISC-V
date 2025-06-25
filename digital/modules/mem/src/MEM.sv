5698d51b9e95bc44d0b1d0390d57b15dd039e4c4
ensariskin <ensariskin@gmail.com>
2025-06-25 23:36:20 +0300

    refactor memory module: rename and update signal names for clarity

--- digital/modules/mem/src/MEM.sv
+++ digital/modules/mem/src/MEM.sv
@@ -1,57 +1,60 @@
 `timescale 1ns/1ns
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 17.06.2022 18:13:38
-// Design Name: 
+// Design Name:
 // Module Name: MEM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module MEM#(parameter size = 32)(
-    input [size-1 : 0] FU_i,
-    input [size-1 : 0] RAM_DATA_i,
-    input [size-1 : 0] PCplus_i,
-    input [size-1 : 0] MEM_result_i,
-    input [11 : 0] Control_Signal_i,
-    
-    output [size-1 : 0] RAM_DATA_o,
-    output [2:0] RAM_DATA_control,
-    output RAM_rw,
-    
-    output [4:0] RD_MEM,
-    output WE_MEM,
-    
-    output [size-1 : 0] FU_o,
-    output [size-1 : 0] MEM_result_o,
-    output [size-1 : 0] PCplus_o,
-    output [7 : 0] Control_Signal_o);
-    
-    
-    
-    assign RAM_DATA_o = RAM_DATA_i;
-    assign RAM_DATA_control = Control_Signal_i[2:0];
-    assign RAM_rw = Control_Signal_i[3] & ~Control_Signal_i[6];
-    assign FU_o = FU_i;
-    assign MEM_result_o = MEM_result_i;
-    assign PCplus_o = PCplus_i;
-    assign Control_Signal_o = Control_Signal_i[11:4];
-    
-    
-    assign RD_MEM = Control_Signal_i[11:7];
-    assign WE_MEM = Control_Signal_i[6];
-    
-   
+module mem_stage #(parameter size = 32)(
+
+    input  logic [size-1 : 0] execute_result_i,
+    input  logic [size-1 : 0] store_data_i,
+    input  logic [11 : 0] control_signal_i,
+
+    input  logic [size-1 : 0] load_data_i,  // from memory
+
+    output logic [size-1 : 0] store_data_o,
+    output logic [2:0] data_mem_width_sel, // to memory
+    output logic data_mem_rw,
+
+    output logic [size-1 : 0] execute_result_o,
+    output logic [size-1 : 0] load_data_o,    // to write back stage
+
+    output logic [7 : 0] control_signal_o,
+
+    output logic [4:0] mem_stage_destination,
+    output logic       mem_stage_we
+    );
+
+
+
+    assign store_data_o = store_data_i;
+
+    assign data_mem_width_sel = control_signal_i[2:0];
+    assign data_mem_rw      = control_signal_i[3] & ~control_signal_i[6]; // todo check the logic for read/write
+    assign execute_result_o = execute_result_i;
+    assign load_data_o = load_data_i;
+
+    assign control_signal_o = control_signal_i[11:4];
+
+
+    assign mem_stage_destination = control_signal_i[11:7];
+    assign mem_stage_we          = control_signal_i[6];
+
+
 endmodule
