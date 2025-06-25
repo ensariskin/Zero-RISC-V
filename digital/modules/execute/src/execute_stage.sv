616cdd335a8f37df7918a5b6646e8245d82219e4
ensariskin <ensariskin@gmail.com>
2025-06-25 23:32:57 +0300

    added mux to select fu result or pc_save

--- digital/modules/execute/src/execute_stage.sv
+++ digital/modules/execute/src/execute_stage.sv
@@ -19,10 +19,8 @@ module execute_stage #(parameter size = 32)(
     input  logic [1 : 0] data_b_forward_sel,
     input  logic [2 : 0] branch_sel,
 
-    // does we need really all these outputs? TODO : select function unit output or pc_plus (and rename it pc_save) output
-    output logic [size-1 : 0] function_unit_o,
     output logic [size-1 : 0] store_data_o,
-    output logic [size-1 : 0] pc_plus_o,
+    output logic [size-1 : 0] calculated_result_o,
 
     output logic [11 : 0] control_signal_o,
 
@@ -37,17 +35,17 @@ module execute_stage #(parameter size = 32)(
     logic N,Z;
     logic Real_MPC;
 	logic isJALR;
+    logic [size-1 : 0] function_unit_o;
 
-    parametric_mux #(.mem_width(size), .mem_depth(4)) A_mux(
+    parametric_mux #(.mem_width(size), .mem_depth(4)) data_a_mux(
         .addr(data_a_forward_sel),
         .data_in({data_from_mem, data_from_wb, data_from_mem, data_a_i}),
         .data_out(data_a));
 
-   parametric_mux  #(.mem_width(size), .mem_depth(4)) B_mux(
+   parametric_mux  #(.mem_width(size), .mem_depth(4)) dat_b_mux(
         .addr(data_b_forward_sel),
         .data_in({data_from_mem, data_from_wb, data_from_mem, data_b_i}),
         .data_out(data_b));
-
 
     function_unit_alu_shifter #(.size(size)) func_unit(
         .data_a(data_a),
@@ -66,15 +64,19 @@ module execute_stage #(parameter size = 32)(
         .MPC(Real_MPC),
         .JALR(isJALR));
 
-	parametric_mux #(.mem_width(size), .mem_depth(2)) correction_mux(
+	parametric_mux #(.mem_width(size), .mem_depth(2)) pc_correction_mux(
 		.addr(isJALR),
 		.data_in({function_unit_o, pc_plus_i}),
 		.data_out(correct_pc));
 
+    parametric_mux #(.mem_width(size), .mem_depth(2)) pc_mux(
+        .addr(control_signal_i[5]),                             //save pc value or function unit output
+        .data_in({pc_plus_i, function_unit_o}),
+        .data_out(calculated_result_o));
+
     assign store_data_o = store_data_i;
-    assign pc_plus_o = pc_plus_i;
     assign control_signal_o = {control_signal_i[25:21],control_signal_i[6:0]};
-    assign rs1_addr = control_signal_i[15:11];
+    assign rs1_addr = control_signal_i[15:11]; // todo we can handle this at top level
     assign rs2_addr = control_signal_i[20:16];
     assign misprediction_o =  (Real_MPC ^  branch_prediction_i);
 endmodule
