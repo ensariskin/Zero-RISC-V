388add693342a1c7a6e04169cbc1e80aa8d8c350
ensariskin <ensariskin@gmail.com>
2025-07-01 23:57:28 +0300

    moved pipeline registers into stgae modules (fetch and decode for now)

--- digital/modules/decode_stage/src/decode_stage.sv
+++ digital/modules/decode_stage/src/decode_stage.sv
@@ -25,25 +25,37 @@ module decode_stage #(parameter size = 32)(
     input logic [size-1 : 0] pc_plus_i,
     input logic branch_perediction_i,
 
+    // Pipeline control signals
+    input logic flush,
+
     //Signal from writeback stage
     input logic [5:0] control_signal_wb,
     input logic [size-1:0] data_in_wb, // TODO : consider renaming
 
+    // Pipeline outputs (ID/EX register outputs)
     output logic branch_prediction_o,
-    output logic [size-1 : 0] data_a,
-    output logic [size-1 : 0] data_b,
-    output logic [size-1 : 0] store_data,
+    output logic [size-1 : 0] data_a_o,
+    output logic [size-1 : 0] data_b_o,
+    output logic [size-1 : 0] store_data_o,
     output logic [size-1 : 0] pc_plus_o,
-    output logic [25 : 0] control_signal,
-    output logic [2:0] branch_sel);
+    output logic [25 : 0] control_signal_o,
+    output logic [2:0] branch_sel_o);
 
+    // Internal signals
     logic [size-1 : 0] reg_b_value;
+    logic branch_prediction_internal;
+    logic [size-1 : 0] data_a_internal;
+    logic [size-1 : 0] data_b_internal;
+    logic [size-1 : 0] store_data_internal;
+    logic [size-1 : 0] pc_plus_internal;
+    logic [25 : 0] control_signal_internal;
+    logic [2:0] branch_sel_internal;
 
     rv32i_decoder #(.size(size)) decoder(
         .instruction(i_instruction),
         .buble(buble),
-        .branch_sel(branch_sel),      // TODO : consider moving branch_sel to control signals
-        .control_word(control_signal) // TODO : consider using interface for control signals
+        .branch_sel(branch_sel_internal),      // TODO : consider moving branch_sel to control signals
+        .control_word(control_signal_internal) // TODO : consider using interface for control signals
     );
 
     register_file #(.mem_width(size),.mem_depth(size)) RegFile(
@@ -51,19 +63,50 @@ module decode_stage #(parameter size = 32)(
         .reset(reset),
         .we(control_signal_wb[0]),
         .rd_in(data_in_wb),
-        .a_select(control_signal[15:11]),
-        .b_select(control_signal[20:16]),
+        .a_select(control_signal_internal[15:11]),
+        .b_select(control_signal_internal[20:16]),
         .write_addr(control_signal_wb[5:1]),
-        .a_out(data_a),
+        .a_out(data_a_internal),
         .b_out(reg_b_value));
 
     parametric_mux #(.mem_width(size), .mem_depth(2)) Mux_B(
-        .addr(control_signal[3]),
+        .addr(control_signal_internal[3]),
         .data_in({immediate_i, reg_b_value}),
-        .data_out(data_b));
+        .data_out(data_b_internal));
 
+    // Internal assignments
+    assign branch_prediction_internal = branch_perediction_i;
+    assign store_data_internal = reg_b_value;
+    assign pc_plus_internal = pc_plus_i;
 
-    assign branch_prediction_o = branch_perediction_i;
-    assign store_data = reg_b_value;
-    assign pc_plus_o = pc_plus_i;
+    // ID/EX Pipeline Register
+    always @(posedge clk or negedge reset) begin
+        if (!reset) begin
+            branch_prediction_o <= 1'b0;
+            data_a_o <= {size{1'b0}};
+            data_b_o <= {size{1'b0}};
+            store_data_o <= {size{1'b0}};
+            pc_plus_o <= {size{1'b0}};
+            control_signal_o <= {26{1'b0}};
+            branch_sel_o <= 3'b000;
+        end else begin
+            if(flush) begin
+                branch_prediction_o <= 1'b0;
+                data_a_o <= {size{1'b0}};
+                data_b_o <= {size{1'b0}};
+                store_data_o <= {size{1'b0}};
+                pc_plus_o <= {size{1'b0}};
+                control_signal_o <= {26{1'b0}};
+                branch_sel_o <= 3'b000;
+            end else begin
+                branch_prediction_o <= branch_prediction_internal;
+                data_a_o <= data_a_internal;
+                data_b_o <= data_b_internal;
+                store_data_o <= store_data_internal;
+                pc_plus_o <= pc_plus_internal;
+                control_signal_o <= control_signal_internal;
+                branch_sel_o <= branch_sel_internal;
+            end
+        end
+    end
 endmodule
--- digital/modules/fetch_stage/src/fetch_stage.sv
+++ digital/modules/fetch_stage/src/fetch_stage.sv
@@ -7,27 +7,36 @@ module fetch_stage#(parameter size = 32)(
     input  logic misprediction,
     input  logic [size-1 : 0] instruction_i,
 
-	input  logic [size-1 : 0] correct_pc,
-    output logic [size-1 : 0] instruction_o,
-    output logic [size-1 : 0] current_pc,
-    output logic [size-1 : 0] imm_o,
-    output logic [size-1 : 0] pc_save,
-    output branch_prediction);
+    // Pipeline control signals
+    input  logic flush,
 
+	input  logic [size-1 : 0] correct_pc,
+    output logic [size-1 : 0] current_pc,
+
+    // Pipeline outputs (IF/ID register outputs)
+    output logic [size-1 : 0] instruction_o,
+    output logic [size-1 : 0] imm_o,
+    output logic [size-1 : 0] pc_plus_o,
+    output logic branch_prediction_o);
+
+    // Internal signals
     logic jump;
     logic [size-1 : 0] imm;
 	logic jalr;
+    logic [size-1 : 0] pc_plus_internal;
 
+    // Immediate decoder
     early_stage_immediate_decoder  early_stage_imm_dec(
         .instruction(instruction_i),
         .imm_o(imm));
 
+    // Jump controller
     jump_controller #(.size(size)) jump_controller(
         .instruction(instruction_i),
-        //.isValid(isValid),
         .jump(jump),
 		.jalr(jalr));
 
+    // Program counter control
     program_counter_ctrl PC(
         .clk(clk),
         .reset(reset),
@@ -38,10 +47,29 @@ module fetch_stage#(parameter size = 32)(
 		.misprediction(misprediction),
         .imm_i(imm),
         .current_pc(current_pc),
-        .pc_save(pc_save));
+        .pc_save(pc_plus_internal));
 
-    assign instruction_o = instruction_i;  // to avoid warning feedtrough, consider moving pipeline stage into modules
-    assign imm_o = imm;
-    assign branch_prediction = jump;
+    // IF/ID Pipeline Register
+    always @(posedge clk or negedge reset)
+    begin
+        if (!reset) begin
+            instruction_o <= 0;
+            imm_o <= 0;
+            pc_plus_o <= 0;
+            branch_prediction_o <= 0;
+        end else if (~buble) begin
+            if(flush) begin
+                instruction_o <= 0;
+                imm_o <= 0;
+                pc_plus_o <= 0;
+                branch_prediction_o <= 0;
+            end else begin
+                instruction_o <= instruction_i;
+                imm_o <= imm;
+                pc_plus_o <= pc_plus_internal;
+                branch_prediction_o <= jump;
+            end
+        end
+    end
 
 endmodule
