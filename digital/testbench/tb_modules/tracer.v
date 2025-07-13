b3e2a4678b58f5bc540fb317ab6c54e14278f68e
ensariskin <ensariskin@gmail.com>
2025-07-14 00:29:13 +0300

    pushing log files (can be redo)

--- digital/testbench/tb_modules/tracer.v
+++ digital/testbench/tb_modules/tracer.v
@@ -18,8 +18,9 @@ initial begin
 file_pointer = $fopen("../core_logs/trace.log", "w"); //The file is normally located in <vivado-dir>\HornetRISCV-vivado.sim\sim_1\behav\xsim, so let's use relative path to move it to the main folder
     forever begin
         @(posedge valid); //This is required otherwise testbench ignores the update signal
-        $fwrite(file_pointer, "0x%8h (0x%8h)", pc, instr);
+        
         if (is_store) begin
+            $fwrite(file_pointer, "0x%8h (0x%8h)", pc, instr);
             if(mem_size == 2'b00) begin
                 $fwrite(file_pointer, " mem 0x%8h 0x%2h", mem_addr, mem_data);
             end
@@ -32,15 +33,16 @@ file_pointer = $fopen("../core_logs/trace.log", "w"); //The file is normally loc
         end
         else begin
             if (!is_float) begin
-                if (reg_addr == 0) begin
-                    //$fwrite(file_pointer, "0x%8h (0x%8h)", pc, instr); Do nothing, as we moved the pc/instr writing out of the if-else tree
-                end else begin
+                if (reg_addr != 0) begin
+                    
                     if (reg_addr > 9) begin
+                        $fwrite(file_pointer, "0x%8h (0x%8h)", pc, instr);
                         $fwrite(file_pointer, " x%0d 0x%8h", reg_addr, reg_data);
                         if (is_load) begin
                             $fwrite(file_pointer, " mem 0x%8h", mem_addr);
                         end
                     end else begin
+                        $fwrite(file_pointer, "0x%8h (0x%8h)", pc, instr);
                         $fwrite(file_pointer, " x%0d  0x%8h", reg_addr, reg_data);
                         if (is_load) begin
                             $fwrite(file_pointer, " mem 0x%8h", mem_addr);
